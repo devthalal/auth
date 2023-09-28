b6958ae371231ff95be8f9dce58dd1091df015d2
devthalal <thalal.hassan@neoito.com>
2023-09-28 14:28:01 +0530

    feat: Initial commit

---
+++ ._bb_/elements_emulator/federation-expose.js
@@ -0,0 +1,9 @@
+export default {
+  "./auth_fe_login": "./src/auth_fe_login/auth_fe_login",
+  "./auth_fe_login_layout": "./src/auth_fe_login_layout/auth_fe_login_layout",
+  "./auth_fe_reset_password": "./src/auth_fe_reset_password/auth_fe_reset_password",
+  "./auth_fe_verify_email": "./src/auth_fe_verify_email/auth_fe_verify_email",
+  "./auth_fe_forgot_password": "./src/auth_fe_forgot_password/auth_fe_forgot_password",
+  "./auth_fe_signup": "./src/auth_fe_signup/auth_fe_signup",
+  "./auth_fe_verify_otp": "./src/auth_fe_verify_otp/auth_fe_verify_otp"
+}
---
+++ ._bb_/elements_emulator/federation-shared.js
@@ -0,0 +1,48 @@
+export default {
+  "react": {
+    "import": "react",
+    "shareKey": "react",
+    "shareScope": "default",
+    "singleton": true,
+    "requiredVersion": "^18.2.0"
+  },
+  "react-dom": {
+    "import": "react-dom",
+    "shareKey": "react-dom",
+    "shareScope": "default",
+    "singleton": true,
+    "requiredVersion": "^18.2.0"
+  },
+  "react-redux": {
+    "import": "react-redux",
+    "shareKey": "react-redux",
+    "shareScope": "default",
+    "singleton": true,
+    "version": "^7.2.5"
+  },
+  "react-router-dom": {
+    "import": "react-router-dom",
+    "shareKey": "react-router-dom",
+    "shareScope": "default",
+    "singleton": true,
+    "version": "^6.9.0"
+  },
+  "@appblocks/js-sdk": {
+    "import": "@appblocks/js-sdk",
+    "shareKey": "@appblocks/js-sdk",
+    "shareScope": "default",
+    "singleton": true,
+    "version": "^0.0.11"
+  },
+  "react-query": {
+    "import": "react-query",
+    "shareKey": "react-query",
+    "shareScope": "default",
+    "singleton": true,
+    "version": "^3.39.2"
+  },
+  "state-pool": {
+    "requiredVersion": "^0.8.1",
+    "singleton": true
+  }
+}
